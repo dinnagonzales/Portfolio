a9b98d783e8ac3c3892e62f28c74c2983ec5fd60
dinnagonzales <dinnagonzales.05@gmail.com>
2020-04-07 23:04:24 -0700

    Skeleton

---
+++ webpack.dev.config.js
@@ -0,0 +1,71 @@
+const path = require('path'); //
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: { // Output file - generated file
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dev'),
+        publicPath: '', // Tells webpack where all generated files are found
+        // publicPath: 'http://dinnagonzales.com/',                //online
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: path.resolve(__dirname, './dev'),
+        index: 'index.html',
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.hbs$/,
+                use:{
+                    loader: 'handlebars-loader',
+                },
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['transform-class-properties'],
+                    },
+                },
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            }    
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(), // Clean output.path folder
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'Dinna Gonzales',
+            chunks: ['main'],
+            template: 'src/page-template.hbs',
+        }),
+    ],
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,79 @@
+const path = require('path'); //
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: { // Output file - generated file
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, './docs'),
+        publicPath: '', // Tells webpack where all generated files are found
+        // publicPath: 'http://dinnagonzales.com/',                //online
+    },
+    mode: 'production',
+    optimization: {
+        namedModules: true,
+        namedChunks: true,
+        splitChunks: {
+            chunks: 'all',
+            minSize: 10000,
+            automaticNameDelimiter: '_',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.hbs$/,
+                use: {
+                    loader: 'handlebars-loader',
+                },
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['transform-class-properties'],
+                    },
+                },
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            }  
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new CleanWebpackPlugin(), // Clean output.path folder
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'Dinna Gonzales',
+            chunks: ['main', 'vendors_main'],
+            template: 'src/page-template.hbs',
+        }),
+    ],
+};
