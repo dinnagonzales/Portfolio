076af9ae748836f0cce92a4c6847a5f98eee4b23
dinnagonzales <dinnagonzales.05@gmail.com>
2020-04-08 22:00:11 -0700

    Add meta tag for responsiveness + ad dmedia queries

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { media } from './media';
 
 const theme = {
     beige: '#F9FAF4',
@@ -18,32 +19,51 @@ const secondaryFont = css`{
 
 export const Body = styled.main`
     ${primaryFont};
-    padding: 150px;
-    margin: 50px auto 150px;
+    padding: 20px;
+    margin: 50px auto;
+   
+    ${media.mediumUp`
+        padding: 150px;
+        margin: 50px auto 150px;
+    `};
 `;
 
 export const Section = styled.section`
     text-align: center;
     .logo{
-        width: 80px;
+        width: 50px;
+        ${media.mediumUp`
+            width: 80px;
+        `};
     }
+
+    h1{
+        margin: -5px 0 0;
+        ${secondaryFont};
+        color: ${theme.olive};
+        font-size: 40px;
+        white-space: nowrap;
+       
+        ${media.mediumUp`
+            letter-spacing: 5px;
+            font-size: 90px;
+        `};
+    }
+    
     h2{
         ${primaryFont};
         text-transform: uppercase;
         color: ${theme.coral};
-        font-size: 25px;
+        font-size: 12px;
         letter-spacing: 8px;
-        margin: 0 0 35px;
+        margin: 0 0 15px;
         -webkit-text-emphasis-style: open;
         white-space: nowrap;
-    }
-    h1{
-        letter-spacing: 5px;
-        margin: -5px 0 0;
-        ${secondaryFont};
-        color: ${theme.olive};
-        font-size: 90px;
-        white-space: nowrap;
+
+        ${media.mediumUp`
+            font-size: 25px;
+            margin: 0 0 35px;
+        `};
     }
 `;
 
@@ -51,8 +71,13 @@ export const LinksSection = styled(Section)`
     a{
         cursor: pointer;
         display: inline-block;
-        margin: 25px;
-        width: 80px;
+        margin: 15px;
+        width: 30px;
+
+        ${media.mediumUp`
+            margin: 25px;
+            width: 80px;
+        `};
 
         img{
             transition: width 0.2s ease;
---
+++ src/styles/media.js
@@ -0,0 +1,42 @@
+import { css } from "styled-components";
+
+/*
+  ${media.mediumUp``};
+ */
+export const media = {
+    small: (...args) => css`
+        @media (max-width: 767px) {
+        ${css(...args)};
+        }
+    `,
+    mediumDown: (...args) => css`
+        @media (max-width: 992px){
+        ${css(...args)};
+        }
+    `,
+    medium: (...args) => css`
+        @media (min-width: 768px) and (max-width: 992px) {
+        ${css(...args)};
+        }
+    `,
+    mediumUp: (...args) => css`
+        @media (min-width: 768px){
+        ${css(...args)};
+        }
+    `,
+    large: (...args) => css`
+        @media (min-width: 992px) and (max-width: 1200px) {
+        ${css(...args)};
+        }
+    `,
+    largeUp: (...args) => css`
+        @media (min-width: 992px){
+        ${css(...args)};
+        }
+    `,
+    xLargeUp: (...args) => css`
+        @media (min-width: 1200px) {
+          ${css(...args)};
+        }
+    `,
+};
