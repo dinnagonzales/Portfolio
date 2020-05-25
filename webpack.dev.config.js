f0880fd4fc0540a9eacfe2e86579135ccab3f6c1
dinnagonzales <dinnagonzales.05@gmail.com>
2020-05-25 10:22:13 -0700

    5-25: Add autoprefixer

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -28,11 +28,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
             },
             {
                 test: /\.hbs$/,
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -32,11 +32,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
             },
             {
                 test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
             },
             {
                 test: /\.hbs$/,
